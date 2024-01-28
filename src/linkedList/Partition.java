9cb53373ef8b5fdc7a315289de52f8dde42e4c2a
Pallavi Rushabheswara <pallavi@Pallavis-MBP.fios-router.home>
2024-01-27 23:39:30 -0500

    LinkedList - Partition

---
+++ src/linkedList/Partition.java
@@ -0,0 +1,31 @@
+package linkedList;
+
+public class Partition {
+    public static void main(String[] args){
+        int x = 5;
+        SinglyLinkedListIntNode node = new SinglyLinkedListIntNode(1);
+        SinglyLinkedListIntNode lessThan = null;
+        SinglyLinkedListIntNode greaterThanOrEqual = null;
+
+        while(node != null){
+            if(node.data < x){
+                if(lessThan == null)
+                    lessThan = node;
+                else {
+                    lessThan.next = node;
+                    lessThan = lessThan.next;
+                }
+            }
+            else {
+                if(greaterThanOrEqual == null)
+                    greaterThanOrEqual = node;
+                else {
+                    greaterThanOrEqual.next = node;
+                    greaterThanOrEqual = greaterThanOrEqual.next;
+                }
+            }
+            node = node.next;
+        }
+        lessThan.next = greaterThanOrEqual;
+    }
+}
